b764485ef2ca691fe2c22ba434bacd0b724c603f
ehunaj <56645834+ehunaj@users.noreply.github.com>
2020-01-27 17:10:30 +0800

    Update server.py

--- linepy/server.py
+++ linepy/server.py
@@ -3,6 +3,7 @@ from akad.ttypes import ApplicationType
 import re, json, requests, urllib
 
 class LineServer(object):
+s Config(object):
     LINE_HOST_DOMAIN            = 'https://gd2.line.naver.jp'
     LINE_OBS_DOMAIN             = 'https://obs-sg.line-apps.com'
     LINE_TIMELINE_API           = 'https://gd2.line.naver.jp/mh/api'
@@ -17,24 +18,61 @@ class LineServer(object):
     LINE_CERTIFICATE_PATH       = '/Q'
     LINE_CHAN_QUERY_PATH        = '/CH4'
     LINE_SQUARE_QUERY_PATH      = '/SQS1'
+    LINE_SHOP_QUERY_PATH        = '/SHOP4'
+    LINE_LIFF_QUERY_PATH        = '/LIFF1'
 
     CHANNEL_ID = {
         'LINE_TIMELINE': '1341209850',
         'LINE_WEBTOON': '1401600689',
         'LINE_TODAY': '1518712866',
         'LINE_STORE': '1376922440',
-        'LINE_MUSIC': '1381425814'
+        'LINE_MUSIC': '1381425814',
+        'LINE_SERVICES': '1459630796'
     }
 
-    USER_AGENT  = 'Line/7.14.0'
+    APP_VERSION = {
+        'ANDROID': '8.14.2',
+        'IOS': '9.18.1' #8.14.2',
+        'ANDROIDLITE': '2.1.0',
+        'BIZANDROID': '1.7.2',
+        'BIZIOS': '1.7.5',
+        'BIZWEB': '1.0.22',
+        'DESKTOPWIN': '5.9.0',
+        'DESKTOPMAC': '5.9.0',
+        'IOSIPAD': '8.14.2',
+        'CHROMEOS': '2.1.5',
+        'WIN10': '5.5.5',
+        'DEFAULT': '8.11.0'
+    }
+    USER_AGENT  = 'Line/9.18.1' #7.14.0'
     APP_TYPE    = ApplicationType.IOS
-    APP_NAME    = 'IOSIPAD\x097.14.0\x09iPhone_OS\x0911.2.5'
-    #APP_NAME    = 'CHROMEOS\t7.14.1\tChrome_OS\t1'
-    #APP_NAME    = 'DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0' #IOSIPAD\t7.14.0\tiPhone OS\t11.2.5'
+    APP_TYPE    = 'IOS'
+   # APP_VER     = APP_VERSION[APP_TYPE] if APP_TYPE in APP_VERSION else APP_VERSION['DEFAULT']
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = 'Ehun'
+    SYSTEM_VER  = '11.2.5'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
+
+    #def __init__(self, appType=None):
+       # if appType:
+            #self.APP_TYPE = appType
+            #self.APP_VER = self.APP_VERSION[self.APP_TYPE] if self.APP_TYPE in self.APP_VERSION else self.APP_VERSION['DEFAULT']
+        #self.APP_NAME = '%s\t%s\t%s\t%s' % (self.APP_TYPE, self.APP_VER, self.SYSTEM_NAME, self.SYSTEM_VER)
+        #self.USER_AGENT = 'Line/%s' % self.APP_VER
+
+
+  #  }
+
+   # USER_AGENT  = 'Line/7.14.0'
+   # APP_TYPE    = ApplicationType.IOS
+ #   APP_NAME    = 'IOSIPAD\x097.14.0\x09iPhone_OS\x0911.2.5'
+    #APP_NAME    = 'CHROMEOS\t7.14.1\tChrome_OS\t1'
+    #APP_NAME    = 'DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0' #IOSIPAD\t7.14.0\tiPhone OS\t11.2.5'
+  #  CARRIER     = '51089, 1-0'
+    #SYSTEM_NAME = 'Ehun'
+   # IP_ADDR     = '8.8.8.8'
+  #  EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
     _session    = requests.session()
     channelHeaders  = {}
